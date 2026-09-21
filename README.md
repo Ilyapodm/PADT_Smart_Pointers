084e239300cd63fc71f13d2f7454d48c7fa59b6e
Ilya Podmarkov <shmell.letitt12@gmail.com>
2026-09-21 12:11:39 +0300

    feat: make cmake arch + minor changes in unique_ptr

--- README.md
+++ README.md
@@ -20,7 +20,7 @@
 
 ## API Overview
 
-Оператор `*` - разыменовывает указатель.
+- Оператор `*` - разыменовывает указатель.
 Оператор `->` - 
 
 `get` возвращает сырой указатель, но не передает владение. Вызывающий код не должен вызывать `delete`
@@ -30,6 +30,8 @@
 `reset` - корректно завершает текущее владение и принимает новое
 
 ## Design Decisions
+
+Why don't use exceptions in UniquePtr? - UniquePtr is a **thin** wrapper of the raw pointer, so user have to use it like raw pointer (carefully) 
 
 ### Template Specializations
 
--- include/unique_ptr.hpp
+++ include/unique_ptr.hpp
@@ -1,11 +1,15 @@
+#include <cstddef>
 
 template <typename T>
 class UniquePtr {
 public:
-    UniquePtr(T* ptr) : ptr{ptr} {}
-    ~UniquePtr() {delete ptr;}  // FIXME
+    explicit UniquePtr(T* ptr = nullptr) : ptr{ptr} {}
+    
+    ~UniquePtr() {
+        delete ptr;
+    }
 
-    // copying is forbedden for unique_ptr
+    // copying is forbidden for unique_ptr
     UniquePtr(const UniquePtr& other) = delete;
     UniquePtr& operator=(const UniquePtr& other) = delete;
 
@@ -13,9 +17,8 @@ public:
         other.ptr = nullptr;
     }
 
-    UniquePtr& operator=(UniquePtr&& other) {
+    UniquePtr& operator=(UniquePtr&& other) noexcept {
         if (this != &other) {
-            // FIXME
             delete ptr;
 
             ptr = other.ptr;
@@ -25,6 +28,102 @@ public:
         return *this;
     }
 
+    T& operator*() const {
+        return *ptr;
+    }
+
+    T* operator->() const {
+        return ptr;
+    }
+
+    explicit operator bool() const noexcept {
+        return ptr != nullptr;
+    }
+
+    T* get() const noexcept {
+        return ptr;
+    }
+
+    T* release() noexcept {
+        T* result = ptr;
+        ptr = nullptr;
+        return result;
+    }
+
+    void reset(T* ptr = nullptr) noexcept {
+        if (this->ptr != ptr) {
+            delete this->ptr;
+            this->ptr = ptr;
+        }
+    }
+
+    void swap(UniquePtr<T>& other) noexcept {
+        T* tmp = ptr;
+        ptr = other.ptr;
+        other.ptr = tmp;
+    }
+private:
+    T* ptr;
+};
+
+template <typename T>
+class UniquePtr<T[]> {
+public:
+    explicit UniquePtr(T* ptr = nullptr) : ptr{ptr} {}
+    
+    ~UniquePtr() {
+        delete[] ptr;
+    }
+
+    // copying is forbidden for unique_ptr
+    UniquePtr(const UniquePtr& other) = delete;
+    UniquePtr& operator=(const UniquePtr& other) = delete;
+
+    UniquePtr(UniquePtr&& other) noexcept : ptr{other.ptr} {
+        other.ptr = nullptr;
+    }
+
+    UniquePtr& operator=(UniquePtr&& other) noexcept {
+        if (this != &other) {
+            delete[] ptr;
+
+            ptr = other.ptr;
+            other.ptr = nullptr;
+        }
+
+        return *this;
+    }
+
+    T& operator[](std::size_t idx) const {
+        return ptr[idx];
+    }
+
+    explicit operator bool() const noexcept {
+        return ptr != nullptr;
+    }
+
+    T* get() const noexcept {
+        return ptr;
+    }
+
+    T* release() noexcept {
+        T* result = ptr;
+        ptr = nullptr;
+        return result;
+    }
+
+    void reset(T* ptr = nullptr) noexcept {
+        if (this->ptr != ptr) {
+            delete[] this->ptr;
+            this->ptr = ptr;
+        }
+    }
+
+    void swap(UniquePtr<T[]>& other) noexcept {
+        T* tmp = ptr;
+        ptr = other.ptr;
+        other.ptr = tmp;
+    }
 private:
     T* ptr;
 };
