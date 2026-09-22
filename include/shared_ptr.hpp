b1db471c66b47b50652a3f22f4f1f2ea6f3cf0cd
Ilya Podmarkov <shmell.letitt12@gmail.com>
2026-09-22 11:30:55 +0300

    feat: add constructors for SharedPtr<T>

--- include/shared_ptr.hpp
+++ include/shared_ptr.hpp
@@ -1,1 +1,75 @@
-// now empty here
+#include <cstddef>
+
+template <typename T>
+class SharedPtr{
+public:
+    SharedPtr() : ptr{nullptr}, count{nullptr} {}
+
+    SharedPtr(T* ptr) : ptr{ptr}, count{new std::size_t} {
+        ++(*count);
+    }
+
+    ~SharedPtr() {
+        release_ownership();
+    }
+
+    SharedPtr(const SharedPtr<T>& other) : ptr{other.ptr}, count{other.count} {
+        ++(*count);
+    }
+
+    SharedPtr<T>& operator=(const SharedPtr<T>& other) {
+        if (this != &other) {
+            // this my own the data, so we need to resease the ownership
+            release_ownership();
+            ptr = other.ptr;
+            count = other.count;
+            ++(*count);
+        }
+        return *this;
+    }
+
+    SharedPtr(SharedPtr<T>&& other) : ptr{other.ptr}, count{other.count} {
+        other.ptr = nullptr;
+        other.count = nullptr;
+        // don't need to increase count, because we "move" (just change the owner), don't copy
+    }
+
+    SharedPtr<T>& operator=(SharedPtr<T>&& other) {
+        if (this != &other) {
+            release_ownership();
+            
+            ptr = other.ptr;
+            count = other.count;
+            
+            other.ptr = nullptr;
+            other.count = nullptr;
+        }
+
+        return *this;
+    }
+
+
+
+
+
+private:
+    T* ptr;
+    std::size_t* count;
+
+    void release_ownership() noexcept {
+        // "this ptr doesn't belong to any ptrs 'group'"
+        if (count == nullptr) {
+            return;  // this way we don't own any data, so nothing to release
+        }
+
+        --(*count);
+
+        if (*count == 0) {
+            delete ptr;
+            delete count;
+        }
+
+        ptr = nullptr;
+        count = nullptr;
+    }
+};
